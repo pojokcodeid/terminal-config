52b1982b17dcda2ce154f545234303a6af77bf6c
Asep Komarudin <68836805+pojokcodeid@users.noreply.github.com>
2023-04-11 19:30:08 +0700

    Update .hyper.js

--- hyper-terminal/.hyper.js
+++ hyper-terminal/.hyper.js
@@ -85,7 +85,7 @@ module.exports = {
         showWindowControls: '',
         // custom padding (CSS format, i.e.: `top right bottom left`)
         //padding: '12px 14px',
-        padding: '1px 0px',
+        padding: '2px 2px',
         // the full list. if you're going to provide the full color palette,
         // including the 6 x 6 color cubes and the grayscale map, just provide
         // an array here instead of a color map object
