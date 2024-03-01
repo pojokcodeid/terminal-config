7ed9843647e2aa07847809bf3bd51d1488b5ed2e
Asep Komarudin <68836805+pojokcodeid@users.noreply.github.com>
2024-03-01 21:06:47 +0700

    Update .hyper.js

--- hyper-terminal/.hyper.js
+++ hyper-terminal/.hyper.js
@@ -8,15 +8,12 @@ module.exports = {
         // or `'canary'` for less polished but more frequent updates
         updateChannel: 'stable',
         // default font size in pixels for all tabs
-        fontSize: 24,
+        fontSize: 17,
         // font family with optional fallbacks
-        // fontFamily: '"SauceCodePro Nerd Font"',
-        fontFamily: '"FiraCode Nerd Font"',
+        fontFamily: 'Hasklug Nerd Font',
         // default font weight: 'normal' or 'bold'
-        //fontWeight: 'normal',
         fontWeight: 'Medium',
         // font weight for bold characters: 'normal' or 'bold'
-        //fontWeightBold: 'bold',
         fontWeightBold: 'normal',
         // line height as a relative unit
         lineHeight: 1,
@@ -40,21 +37,12 @@ module.exports = {
         // border color (window, tabs)
         borderColor: '#333',
         // custom CSS to embed in the main window
-        //css: '',
-        // css: `
-        //     .term_fit > .terminal > .xterm-screen {
-        //         width: 100vw !important;
-        //     }
-        //     .term_fit > .terminal > .xterm-screen canvas {
-        //         width: 100vw !important;
-        //     }
-        //     `,
         css: `
             .term_fit > .terminal > .xterm-screen {
                 width: 100vw !important;               
             }
             .term_fit > .terminal > .xterm-screen canvas {
-                width: 100vw !important;
+								width: 100% !important;
                 height: 100% !important;
                 padding-bottom:0px !important;
                 margin-bottom:0px !important;
@@ -71,7 +59,7 @@ module.exports = {
                 padding-bottom:0px !important;
                 margin-bottom:0px !important;
                 bottom:0px !important;
-            }`, 
+            }`,
         // custom CSS to embed in the terminal window
         termCSS: '',
         // set custom startup directory (must be an absolute path)
@@ -84,8 +72,7 @@ module.exports = {
         // default: `true` (without backticks and without quotes) on Windows and Linux, ignored on macOS
         showWindowControls: '',
         // custom padding (CSS format, i.e.: `top right bottom left`)
-        //padding: '12px 14px',
-        padding: '2px 2px',
+        padding: '4px 5px',
         // the full list. if you're going to provide the full color palette,
         // including the 6 x 6 color cubes and the grayscale map, just provide
         // an array here instead of a color map object
@@ -127,8 +114,7 @@ module.exports = {
         //
         // Cygwin
         // - Example: `C:\\cygwin64\\bin\\bash.exe`
-        // shell: 'pwsh.exe',
-        shell: '/bin/bash',
+        shell: 'pwsh.exe',
         // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
         // by default `['--login']` will be used
         shellArgs: ['--login'],
@@ -166,7 +152,8 @@ module.exports = {
         // set to true to preserve working directory when creating splits or tabs
         preserveCWD: true,
         // for advanced config flags please refer to https://hyper.is/#cfg
-        opacity: 0.95,
+				 // for advanced config flags please refer to https://hyper.is/#cfg
+        opacity: 1,
     },
     // a list of plugins to fetch and install from npm
     // format: [@org/]project[#version]
@@ -175,13 +162,13 @@ module.exports = {
     //   `@company/project`
     //   `project#1.0.1`
     plugins: [
-        "hyperterm-chesterish",
-        "hypercwd",
-        "hyper-snazzy",
-        "hyper-opacity",
-        "hyper-tab-icons",
-        "hyperterm-gruvbox-dark",
-    ],
+			"hyperterm-chesterish",
+			"hypercwd",
+			"hyper-snazzy",
+			"hyper-opacity",
+			"hyper-tab-icons",
+			"hyper-tokyo-night",
+		],
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed
